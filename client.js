06406d1896ace510bce9d8ef33d114d90f573937
cryosx <cyruswu.email@gmail.com>
2018-04-21 16:01:20 -1000

    header and body properly split, chunked data accounted for

--- client.js
+++ client.js
@@ -4,6 +4,8 @@ const { URL } = require('url');
 
 const arguments = process.argv.slice(2);
 
+const cliOptions = ['-port', '-p', '-header', '-h', '-method', '-m'];
+
 const methods = [
   'GET',
   'HEAD',
@@ -16,83 +18,61 @@ const methods = [
   'PATCH'
 ];
 
-// const protocols = { 'http:': 80, 'https:': 443 };
 const ports = { 'http:': 80, 'https:': 443 };
 
-// let serverUrl = new URL(process.argv[2]);
+processCli(arguments);
+
+function processCli(arguments) {}
 
 let serverUrl = new URL(validateUrl(process.argv[2]));
 
 let method = validateMethod(process.argv[3]);
 
-// let responseString = '';
-let response = {};
 let chunks = [];
 
-console.log(serverUrl);
 let options = {
-  host: serverUrl.host,
-  port: ports[serverUrl.protocol]
+  host: serverUrl.hostname,
+  port: serverUrl.port || ports[serverUrl.protocol] || 80
 };
 
 const server = net.createConnection(options, (...args) => {
-  console.log('connected to server!');
-
+  console.log('connected');
   server.setEncoding('utf8');
+
   server.write(
     buildRequestHeader(method, serverUrl.pathname, serverUrl.hostname)
   );
 
   server.on('data', data => {
-    // responseString += data;
     chunks.push(data);
-    console.log(data);
     server.end();
   });
 
   server.on('end', () => {
-    console.log(chunks);
+    let response = {};
+
     let responseString = chunks.join('');
-    console.log(responseString);
 
-    let responseLine = responseString.split('\r\n');
-    // console.log(responseLine);
-    let responseHeader = responseLine.slice(0, responseLine.length - 2);
-    let responseBody = responseLine.slice(responseLine.length - 1);
-    responseBody = responseLine[responseLine.length - 4];
-    // console.log(responseLine[responseLine.length - 4]);
+    let responseSplit = responseString.split('\r\n\r\n');
 
-    // let responseLine = responseLine[0].split(' ');
-    // console.log(responseHeader);
-    response[url.host] = responseHeader.reduce(function(acum, curr, index) {
-      if (index === 0) {
-        acum.header = {};
-        let responseLine = curr.split(' ');
-        acum.header.responseLine = {
-          method: responseLine[0],
-          URI: responseLine[1],
-          version: responseLine[2]
-        };
-        return acum;
-      } else {
-        let headerLine = curr.split(': ');
-        acum.header[headerLine[0]] = headerLine[1];
-        return acum;
-      }
-    }, {});
-    response[url.host]['body'] = responseBody;
-    // console.log(response[url.host]);
-    // console.log(response[url.host].body);
-    console.log('disconnected from server');
+    let responseHeader = responseSplit[0];
+    response.header = parseHeader(responseHeader);
+
+    let responseBody = responseSplit[1];
+    if (
+      response.header['Transfer-Encoding'] ||
+      response.header['Transfer-Encoding'] === 'chunked'
+    ) {
+      response.body = parseBody(responseBody, 'chunked');
+    } else {
+      response.body = parseBody(responseBody, false);
+    }
+    console.log(response.body);
   });
-  server.on('close', (...args) => {
-    // console.log(...args);
-  });
+  server.on('close', (...args) => {});
 });
 
-server.on('error', error => {
-  console.log(error);
-});
+server.on('error', error => {});
 
 function validateMethod(method) {
   if (method && methods.includes(method.toUpperCase())) {
@@ -100,15 +80,6 @@ function validateMethod(method) {
   } else {
     return 'GET';
   }
-}
-
-function buildRequestHeader(method, uri, host) {
-  let requestLine = `${method} ${uri} HTTP/1.1\r\n`;
-  let hostLine = `Host: ${host}\r\n`;
-  let dateLine = `Date: ${new Date().toUTCString()}\r\n`;
-  let breakLine = '\r\n';
-  console.log(requestLine + hostLine + dateLine + breakLine);
-  return requestLine + hostLine + dateLine + breakLine;
 }
 
 function validateUrl(url) {
@@ -120,46 +91,45 @@ function validateUrl(url) {
   return url;
 }
 
-// function setUrl(url) {
-//   try {
-//     url = new URL(url);
-//   } catch (error) {
-//     url = new URL(`http://${url}`);
-//   }
-//   return url;
-// }
+function buildRequestHeader(method, uri, host) {
+  let requestLine = `${method} ${uri} HTTP/1.1\r\n`;
+  let hostLine = `Host: ${host}\r\n`;
+  let dateLine = `Date: ${new Date().toUTCString()}\r\n`;
+  let breakLine = '\r\n';
+  // console.log(requestLine + hostLine + dateLine + breakLine);
+  return requestLine + hostLine + dateLine + breakLine;
+}
 
-// function parseUrl(url) {
-//   let host = null;
-//   let protocol = null;
-//   let uri = null;
-//   let port = null;
+function parseHeader(header) {
+  // let temp = responseHeader.split('\r\n');
+  let result = null;
 
-//   if (url.startsWith('https')) {
-//     port = 443;
-//   } else {
-//     port = 80;
-//   }
+  result = header.split('\r\n').reduce(function(acum, curr, index) {
+    if (index === 0) {
+      let statusline = curr.split(' ');
+      acum.status = {
+        version: statusline[0],
+        code: statusline[1],
+        phrase: statusline[2]
+      };
+    } else {
+      let header = curr.split(': ');
+      acum[header[0]] = header[1];
+    }
+    return acum;
+  }, {});
 
-//   if (url.indexOf('://') !== -1) {
-//     let urlSplit = url.split('://');
-//     protocol = urlSplit[0];
-//     url = urlSplit[1];
-//   } else {
-//     protocol = 'http';
-//   }
+  return result;
+}
 
-//   if (url.indexOf('/') !== -1) {
-//     host = url.slice(0, url.indexOf('/'));
-//     uri = url.slice(url.indexOf('/'));
-//   } else {
-//     host = url;
-//     uri = '/';
-//   }
-
-//   return {
-//     host: `${host}`,
-//     uri: uri,
-//     port: port
-//   };
-// }
+function parseBody(body, chunked) {
+  let result = null;
+  if (chunked) {
+    result = body.split('\r\n').reduce(function(acum, curr, index) {
+      return index % 2 === 1 ? acum + curr : acum;
+    }, '');
+  } else {
+    result = body;
+  }
+  return result;
+}
