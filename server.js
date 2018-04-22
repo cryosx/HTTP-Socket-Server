47fd387c540759de8863e3482cb0ca9915af5b6a
cryosx <cyruswu.email@gmail.com>
2018-04-21 18:33:17 -1000

    saving before refactor

--- server.js
+++ server.js
@@ -7,17 +7,14 @@ const port = 8080;
 let header = null;
 
 const server = net.createServer(client => {
-  // console.log('client connected');
-
   client.setEncoding('utf8');
+
   client.on('data', data => {
     let request = data.split('\r\n');
 
     let requestHeader = request.slice(0, request.length - 2);
     let requestBody = request.slice(request.length - 1);
 
-    // let requestLine = request[0].split(' ');
-    // console.log(requestHeader);
     header = requestHeader.reduce(function(acum, curr, index) {
       if (index === 0) {
         let requestLine = curr.split(' ');
@@ -50,67 +47,6 @@ const server = net.createServer(client => {
         )
       );
     }
-
-    // if (uri.file === '/') {
-    //   client.write(
-    //     buildResponse(
-    //       dataPages['index.html'],
-    //       200,
-    //       'html',
-    //       header.request.method
-    //     )
-    //   );
-    // } else if (dataPages.hasOwnProperty(uri.file) && uri.file !== '404.html') {
-    //   client.write(
-    //     buildResponse(dataPages[uri.file], 200, uri.type, header.request.method)
-    //   );
-    // } else {
-    //   client.write(
-    //     buildResponse(
-    //       dataPages['404.html'],
-    //       404,
-    //       uri.type,
-    //       header.request.method
-    //     )
-    //   );
-    // }
-
-    // if (header.request.method === 'HEAD') {
-    //   if (header.request.URI === '/' || header.request.URI === '/index.html') {
-    //     client.write(
-    //       buildResponse(dataPages['index.html'], 200, 'html', 'HEAD')
-    //     );
-    //   } else if (header.request.URI === '/hydrogen.html') {
-    //     client.write(
-    //       buildResponse(dataPages['hydrogen.html'], 200, 'html', 'HEAD')
-    //     );
-    //   } else if (header.request.URI === '/helium.html') {
-    //     client.write(
-    //       buildResponse(dataPages['helium.html'], 200, 'html', 'HEAD')
-    //     );
-    //   } else if (header.request.URI === '/css/styles.css') {
-    //     client.write(
-    //       buildResponse(dataPages['styles.css'], 200, 'css', 'HEAD')
-    //     );
-    //   } else {
-    //     client.write(buildResponse(dataPages['404.html'], 404, 'html', 'HEAD'));
-    //   }
-    // }
-
-    // if (header.request.method === 'GET') {
-    //   if (header.request.URI === '/' || header.request.URI === '/index.html') {
-    //     client.write(buildResponse(dataPages['index.html'], 200, 'html'));
-    //   } else if (header.request.URI === '/hydrogen.html') {
-    //     client.write(buildResponse(dataPages['hydrogen.html'], 200, 'html'));
-    //   } else if (header.request.URI === '/helium.html') {
-    //     client.write(buildResponse(dataPages['helium.html'], 200, 'html'));
-    //   } else if (header.request.URI === '/css/styles.css') {
-    //     client.write(buildResponse(dataPages['styles.css'], 200, 'css'));
-    //   } else {
-    //     client.write(buildResponse(dataPages['404.html'], 404, 'html'));
-    //   }
-    // }
-    // client.end();
   });
   client.on('end', (...args) => {});
   client.on('close', (...args) => {
