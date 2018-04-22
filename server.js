452bc17ecccf3343f655583659cc75c4a3ea89e4
cryosx <cyruswu.email@gmail.com>
2018-04-21 21:22:48 -1000

    iife

--- server.js
+++ server.js
@@ -1,107 +1,116 @@
-const net = require('net');
+(function() {
+  const net = require('net');
 
-const dataPages = require('./data.js');
+  const dataPages = require('./data.js');
 
-const port = 8080;
+  const port = 8080;
 
-let header = null;
+  let header = null;
 
-const server = net.createServer(client => {
-  client.setEncoding('utf8');
+  const server = net.createServer(client => {
+    client.setEncoding('utf8');
 
-  client.on('data', data => {
-    let request = {};
+    client.on('data', data => {
+      let request = {};
 
-    let requestSplit = data.split('\r\n\r\n');
+      let requestSplit = data.split('\r\n\r\n');
 
-    let requestHeader = requestSplit[0];
-    let requestBody = requestSplit[1];
+      let requestHeader = requestSplit[0];
+      let requestBody = requestSplit[1];
 
-    request.header = parseHeader(requestHeader);
-    request.body = requestBody;
+      request.header = parseHeader(requestHeader);
+      request.body = requestBody;
 
-    let uri = parseURI(request.header.requestline.URI);
+      let uri = parseURI(request.header.requestline.URI);
 
-    if (dataPages.hasOwnProperty(uri.uri) && uri.file !== '404.html') {
-      client.write(
-        buildResponse(
-          dataPages[uri.uri],
-          200,
-          uri.type,
-          request.header.requestline.method
-        )
+      if (dataPages.hasOwnProperty(uri.uri) && uri.file !== '404.html') {
+        client.write(
+          buildResponse(
+            dataPages[uri.uri],
+            200,
+            uri.type,
+            request.header.requestline.method
+          )
+        );
+      } else {
+        client.write(
+          buildResponse(
+            dataPages['404.html'],
+            404,
+            uri.type,
+            request.header.requestline.method
+          )
+        );
+      }
+    });
+    client.on('end', (...args) => {});
+    client.on('close', (...args) => {
+      console.log('Client disconnected');
+    });
+  });
+  server.on('error', error => {
+    throw error;
+  });
+  server.listen(port, () => {});
+
+  let responseCodes = { 200: 'OK', 404: 'Not Found' };
+
+  function buildResponse(body, responseCode, type, method) {
+    let responseMessage = responseCodes[responseCode];
+    if (!responseMessage) {
+      responseMessage = 'Not Found';
+    }
+    let responseLine = `HTTP/1.1 ${responseCode} ${responseMessage}\r\n`;
+    let dateLine = `Date: ${new Date().toUTCString()}\r\n`;
+    let contentType = `Content-Type: text/${type}; charset=utf-8\r\n`;
+    let contentLength = `Content-Length: ${body.length}\r\n`;
+    let headerBreak = `\r\n`;
+
+    if (method === 'HEAD') {
+      return (
+        responseLine + dateLine + contentType + contentLength + headerBreak
       );
     } else {
-      client.write(
-        buildResponse(
-          dataPages['404.html'],
-          404,
-          uri.type,
-          request.header.requestline.method
-        )
+      return (
+        responseLine +
+        dateLine +
+        contentType +
+        contentLength +
+        headerBreak +
+        body
       );
     }
-  });
-  client.on('end', (...args) => {});
-  client.on('close', (...args) => {
-    console.log('Client disconnected');
-  });
-});
-server.on('error', error => {
-  throw error;
-});
-server.listen(port, () => {});
-
-let responseCodes = { 200: 'OK', 404: 'Not Found' };
-
-function buildResponse(body, responseCode, type, method) {
-  let responseMessage = responseCodes[responseCode];
-  if (!responseMessage) {
-    responseMessage = 'Not Found';
   }
-  let responseLine = `HTTP/1.1 ${responseCode} ${responseMessage}\r\n`;
-  let dateLine = `Date: ${new Date().toUTCString()}\r\n`;
-  let contentType = `Content-Type: text/${type}; charset=utf-8\r\n`;
-  let contentLength = `Content-Length: ${body.length}\r\n`;
-  let headerBreak = `\r\n`;
 
-  if (method === 'HEAD') {
-    return responseLine + dateLine + contentType + contentLength + headerBreak;
-  } else {
-    return (
-      responseLine + dateLine + contentType + contentLength + headerBreak + body
-    );
+  function parseHeader(header) {
+    let result = header.split('\r\n').reduce(function(acum, curr, index) {
+      if (index === 0) {
+        let requestLine = curr.split(' ');
+        acum['requestline'] = {
+          method: requestLine[0],
+          URI: requestLine[1],
+          version: requestLine[2]
+        };
+      } else {
+        let headerLine = curr.split(': ');
+        acum[headerLine[0]] = headerLine[1];
+      }
+      return acum;
+    }, {});
+    return result;
   }
-}
+  // function parseBody(body) {}
 
-function parseHeader(header) {
-  let result = header.split('\r\n').reduce(function(acum, curr, index) {
-    if (index === 0) {
-      let requestLine = curr.split(' ');
-      acum['requestline'] = {
-        method: requestLine[0],
-        URI: requestLine[1],
-        version: requestLine[2]
-      };
-    } else {
-      let headerLine = curr.split(': ');
-      acum[headerLine[0]] = headerLine[1];
-    }
-    return acum;
-  }, {});
-  return result;
-}
-// function parseBody(body) {}
+  function parseURI(uri) {
+    let index = uri.lastIndexOf('/');
+    let file = index !== -1 ? uri.slice(index + 1, uri.length) : uri;
 
-function parseURI(uri) {
-  let index = uri.lastIndexOf('/');
-  let file = index !== -1 ? uri.slice(index + 1, uri.length) : uri;
-
-  index = uri.lastIndexOf('.');
-  let fileType = index !== -1 ? uri.slice(index + 1, uri.length) : 'html';
-  return {
-    uri: uri,
-    file: file,
-    type: fileType
-  };
-}
+    index = uri.lastIndexOf('.');
+    let fileType = index !== -1 ? uri.slice(index + 1, uri.length) : 'html';
+    return {
+      uri: uri,
+      file: file,
+      type: fileType
+    };
+  }
+})();
