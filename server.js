e546645cac484e15b8750544b6c8af30e6ebcab9
cryosx <cyruswu.email@gmail.com>
2018-04-21 21:19:51 -1000

    cleaned up code

--- server.js
+++ server.js
@@ -10,32 +10,26 @@ const server = net.createServer(client => {
   client.setEncoding('utf8');
 
   client.on('data', data => {
-    let request = data.split('\r\n');
+    let request = {};
 
-    let requestHeader = request.slice(0, request.length - 2);
-    let requestBody = request.slice(request.length - 1);
+    let requestSplit = data.split('\r\n\r\n');
 
-    header = requestHeader.reduce(function(acum, curr, index) {
-      if (index === 0) {
-        let requestLine = curr.split(' ');
-        acum['request'] = {
-          method: requestLine[0],
-          URI: requestLine[1],
-          version: requestLine[2]
-        };
-        return acum;
-      } else {
-        let headerLine = curr.split(': ');
-        acum[headerLine[0]] = headerLine[1];
-        return acum;
-      }
-    }, {});
+    let requestHeader = requestSplit[0];
+    let requestBody = requestSplit[1];
 
-    let uri = parseURI(header.request.URI);
+    request.header = parseHeader(requestHeader);
+    request.body = requestBody;
+
+    let uri = parseURI(request.header.requestline.URI);
 
     if (dataPages.hasOwnProperty(uri.uri) && uri.file !== '404.html') {
       client.write(
-        buildResponse(dataPages[uri.uri], 200, uri.type, header.request.method)
+        buildResponse(
+          dataPages[uri.uri],
+          200,
+          uri.type,
+          request.header.requestline.method
+        )
       );
     } else {
       client.write(
@@ -43,7 +37,7 @@ const server = net.createServer(client => {
           dataPages['404.html'],
           404,
           uri.type,
-          header.request.method
+          request.header.requestline.method
         )
       );
     }
@@ -66,19 +60,38 @@ function buildResponse(body, responseCode, type, method) {
     responseMessage = 'Not Found';
   }
   let responseLine = `HTTP/1.1 ${responseCode} ${responseMessage}\r\n`;
-  let date = `Date: ${new Date().toUTCString()}\r\n`;
+  let dateLine = `Date: ${new Date().toUTCString()}\r\n`;
   let contentType = `Content-Type: text/${type}; charset=utf-8\r\n`;
   let contentLength = `Content-Length: ${body.length}\r\n`;
   let headerBreak = `\r\n`;
 
   if (method === 'HEAD') {
-    return responseLine + date + contentType + contentLength + headerBreak;
+    return responseLine + dateLine + contentType + contentLength + headerBreak;
   } else {
     return (
-      responseLine + date + contentType + contentLength + headerBreak + body
+      responseLine + dateLine + contentType + contentLength + headerBreak + body
     );
   }
 }
+
+function parseHeader(header) {
+  let result = header.split('\r\n').reduce(function(acum, curr, index) {
+    if (index === 0) {
+      let requestLine = curr.split(' ');
+      acum['requestline'] = {
+        method: requestLine[0],
+        URI: requestLine[1],
+        version: requestLine[2]
+      };
+    } else {
+      let headerLine = curr.split(': ');
+      acum[headerLine[0]] = headerLine[1];
+    }
+    return acum;
+  }, {});
+  return result;
+}
+// function parseBody(body) {}
 
 function parseURI(uri) {
   let index = uri.lastIndexOf('/');
